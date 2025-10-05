b9e7292451269123be657af212c68247b2c7eebb
Germán Moreno <germanclarinet03@gmail.com>
2025-10-05 11:43:30 -0500

    Update mason

--- dotfiles/nvim/after/plugin/lsp.lua
+++ dotfiles/nvim/after/plugin/lsp.lua
@@ -3,10 +3,8 @@ lsp.preset("recommended")
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed={
-        'tsserver',
         'eslint',
         'rust_analyzer',
-        'clangd',
         'bashls',
         'denols',
         'jedi_language_server',
